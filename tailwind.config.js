0fb260b27c338355009dc6b8089363e7eec90641
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-04-30 23:53:30 +0600

    added darkmode swap

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [ require('daisyui')],
+    daisyui: {
+      themes: ["light", "dark"],
+    },
+  
 }
 
